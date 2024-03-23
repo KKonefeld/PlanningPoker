50ba27de4f38abf85a5e0d4084aada56bb662f0c
Dezert <68746157+Dezert01@users.noreply.github.com>
2024-03-23 15:19:18 +0100

    Update buttons

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const NotFound: React.FC = () => {
@@ -5,8 +6,8 @@ const NotFound: React.FC = () => {
     <div className="container mx-auto flex flex-col items-center pt-24 text-white">
       <span className="text-[10rem] font-bold leading-none">404</span>
       <h1 className="mb-8">Not Found</h1>
-      <Link className="bg-blue-500 hover:bg-blue-600 rounded-md p-2" href="/">
-        Return Home
+      <Link href="/">
+        <Button>Return Home</Button>
       </Link>
     </div>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,16 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="container h-10 mx-auto  flex flex-col items-center pt-24">
       <h1>Scrum Poker for agile Teams</h1>
       <div className="flex gap-4 mt-8 text-white">
-        <Link
-          className="bg-blue-500 hover:bg-blue-600 rounded-md p-2"
-          href="/rooms"
-        >
-          Active rooms
+        <Link href="/rooms">
+          <Button>Active rooms</Button>
         </Link>
-        <Link
-          className="bg-blue-500 hover:bg-blue-600 rounded-md p-2"
-          href="/create"
-        >
-          Create new room
+        <Link href="/create">
+          <Button>Create new room</Button>
         </Link>
       </div>
     </div>
