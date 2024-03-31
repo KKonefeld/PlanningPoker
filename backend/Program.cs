ba9bcc262a5cffec2be83af6a1918bed4a326a0d
Adam Kopiec <adas073@gmail.com>
2024-03-31 01:53:20 +0100

    Add RoomHub

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using PlanningPoker.Persistence;
 using PlanningPoker.Services.RoomService;
@@ -16,6 +15,7 @@ namespace PlanningPoker
             builder.Services.AddDbContext<PlanningPokerDbContext>(
                 o => o.UseNpgsql(builder.Configuration.GetConnectionString("PlanningPokerDb"))
                 );
+            builder.Services.AddSignalR();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -36,6 +36,8 @@ namespace PlanningPoker
 
 
             app.MapControllers();
+
+            //app.MapHub<RoomHub>("/roomHub");
 
             app.Run();
         }
---
+++ backend/SignalR/Hubs/RoomHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PlanningPoker.SignalR.Hubs
+{
+    public class RoomHub : Hub
+    {
+        public async Task BroadcastRefreshRoomList()
+        {
+            await Clients.All.SendAsync("RefreshRoomList");
+        }
+
+        public async Task BroadcastJoinedRoom(int roomId, string participantName)
+        {
+            await Clients.Group(roomId.ToString()).SendAsync("ParticipantJoined", roomId, participantName);
+        }
+    }
+}
