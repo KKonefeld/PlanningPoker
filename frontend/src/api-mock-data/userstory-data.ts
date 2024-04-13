95bc413487d234cf19b63693eacdaafcb0fc9e16
Dezert <68746157+Dezert01@users.noreply.github.com>
2024-04-13 12:51:11 +0200

    add user story mock data

---
+++ frontend/src/api-mock-data/userstory-data.ts
@@ -0,0 +1,28 @@
+import { UserStory } from "@/model/userstory";
+
+export const userStories: UserStory[] = [
+  {
+    id: 1,
+    title: "US1",
+  },
+  {
+    id: 2,
+    title: "US2",
+  },
+  {
+    id: 3,
+    title: "US3",
+  },
+  {
+    id: 4,
+    title: "US4",
+  },
+  {
+    id: 5,
+    title: "US5",
+  },
+  {
+    id: 6,
+    title: "US6",
+  },
+];
