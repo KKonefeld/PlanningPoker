393208d8df1e0d0053a42820d50de064d4b6c21b
Dezert <68746157+Dezert01@users.noreply.github.com>
2024-06-05 19:14:37 +0200

    add cookie consent

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./navbar";
 import Providers from "./providers";
+import CookiesConsent from "./cookies-consent";
 
 export const metadata: Metadata = {
   title: "Planning Poker",
@@ -15,13 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-background">
+      <body className="relative bg-background">
         <Providers>
           <Navbar />
           <div className="container mx-auto flex flex-col items-center pt-24 text-white">
             {children}
           </div>
         </Providers>
+        <CookiesConsent />
       </body>
     </html>
   );
